54e75ac99c924470e28e5c616ca4b16d27c39f68
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-07 00:23:53 +0530

    static and dynamic metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,26 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const productId = (await params).productId;
+
+  const product = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${productId}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${product}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
 
   return <h1>Details about Product {productId}</h1>;
